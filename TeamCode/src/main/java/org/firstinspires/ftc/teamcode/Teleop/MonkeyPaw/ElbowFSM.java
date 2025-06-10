b36d2ded57603a24002a239a756db4e4a7c09cbb
juniperrobotics <162588533+Avadhootdhole@users.noreply.github.com>
2025-06-10 07:19:51 -0400

    Update ElbowFSM.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/MonkeyPaw/ElbowFSM.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/MonkeyPaw/ElbowFSM.java
@@ -1,4 +1,25 @@
 package org.firstinspires.ftc.teamcode.Teleop.MonkeyPaw;
 
+import org.firstinspires.ftc.teamcode.Core.HWMap;
+import org.firstinspires.ftc.teamcode.Core.Logger;
+import org.firstinspires.ftc.teamcode.Teleop.Wrappers.AxonCRServoWrapper;
+import org.firstinspires.ftc.teamcode.Teleop.Wrappers.AxonServoWrapper;
+
 public class ElbowFSM {
+    private enum ElbowStates{
+        AT_POS,
+        GOING_TO_POS
+    }
+
+    private int targetAngle;
+    private ElbowStates elbowStates;
+    private AxonServoWrapper axonServoWrapper;
+
+    public ElbowFSM (HWMap hwMap){
+
+    }
+
+    public void updateState(){
+
+    }
 }
